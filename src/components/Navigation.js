5bdeadb0b53c48a462bc5f146c074bb289532c04
Michael Jakobsen <michael@rpair.co.uk>
2022-02-05 17:24:28 +0000

    initial commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+function ShowNavigation() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.rpair.co.uk">Home</a>
+          </li>
+          <li>
+            <a href="https://www.rpair.co.uk">About</a>
+          </li>
+          <li>
+            <a href="https://www.rpair.co.uk">Contact</a>
+          </li>
+          <li>
+            <a href="https://www.rpair.co.uk">Projects</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default ShowNavigation;
